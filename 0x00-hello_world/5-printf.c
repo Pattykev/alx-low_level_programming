d4c56c8c6e9c423b15d6c3a02dec7dfa2dd5eb2d
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-16 13:20:47 +0100

    print with printf()

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  *
- *use printf function
+ * Description: use printf function
  *
+ * Return: Always 0(success)
  */
-int main(void){
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, ");
+	return (0);
 }
