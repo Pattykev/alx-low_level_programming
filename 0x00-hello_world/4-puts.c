39bbb09647e691b47b8bd3a537e4a6cfb735df5b
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-16 13:17:44 +0100

    print with puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: A C function that print a message
+ *
+ * Return: Always 0(Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
