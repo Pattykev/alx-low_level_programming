fa16cb65c2a8f8f9d72ad4e8c8c6149ce84f67e5
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-17 21:37:14 +0100

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print 0, 1, - 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0, a = 0, i=1;
+
+	while (digit <= 9)
+	{
+    for(; i<=9 ; i++)
+    {
+		putchar(digit + 48);
+    putchar(i + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		
+      i++;
+	}
+    ++digit;
+  }
+	putchar('\n');
+
+	return (0);
+}
