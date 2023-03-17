cb315fc62a85a3d7074e42b33379bde24bc171af
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-17 09:34:59 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+  
+    /* code */
+  
+  if(n>0)
+        printf("%d is positive\n", n);
+  else if(n<0)
+        printf("%d is negative\n", n);
+  else
+        printf("%d is zero\n", n);
+  
+  return (0);
+  
+}
+    
