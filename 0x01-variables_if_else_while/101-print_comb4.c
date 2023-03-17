3a55a441fa2b81738354ab5842c53a391ba98de3
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-17 21:45:05 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,45 @@
+#include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: print 0, 1, - 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0, a = 2, i=1;
+
+	while (digit <= 7)
+	{
+    for( ; i<=8 ; )
+    {
+      
+      for(; a<=9 ; )
+    {
+		putchar(digit + 48);
+    putchar(i + 48);
+    putchar(a + 48);
+
+		if (digit != 7)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		  a++;
+     
+	}
+      i++;
+		
+
+		
+     
+	}
+    ++digit;
+  }
+	putchar('\n');
+
+	return (0);
