c0dc9c1c2dcb025b622faffa6e462d5d93576482
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-21 09:29:54 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print if the number is a lower character
+ *           
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _abs(int n)
+{
+      if (n < 0)
+      {
+            n=n*(-1);
+      }  
+      return n;
+}
