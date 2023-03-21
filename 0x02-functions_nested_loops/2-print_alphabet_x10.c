73f57b7a1731ddfc8b76f8cad810d23adc5f16d1
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-21 10:48:13 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,18 @@
 #include"main.h"
 
-
 /**
- * main - Entry point
+ * print_alphabet_x10 - function that will print the alphabet 10 times
  *
- * Description: print_alphabet_x10 - function that will print the alphabet 10 times
- *
- * Return: Always 0 (Success)
 */
 
 void print_alphabet_x10(void)
 {
-	char n;
-  	int t;
- 	 for(t = 1; t <= 10; t++)
-  	{
-    		for(n = 'a'; n <= 'z'; n++)
-    		{
-    			 _putchar(n);
-     			
-    		}  
-   		 _putchar('\n');
-   		 
-  	}
+	int line, ch;
+
+	for (line = 0; line <= 9; ++line)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
