55fe769f2e81e2a5aa9903bd7c230c8d0de9feb3
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-21 09:58:33 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,18 +6,18 @@
 
 void jack_bauer(void)
 {
-int hr, min;  
-for(hr = 0; hr <= 23; hr++)
-{
-for(min = 0; min <= 60; min++)
-{
-_putchar((hr / 10) + 48);
-_putchar((hr % 10) + 48);
-_putchar(':');
-_putchar((min / 10) + 48);
-_putchar((min % 10) + 48);
-_putchar('\n');
-}  
-}  
-  return (0);  
-}  
+	int min, hr;
+
+	for (hr = 0; hr <= 23; ++hr)
+	{
+		for (min = 0; min <= 59; ++min)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
