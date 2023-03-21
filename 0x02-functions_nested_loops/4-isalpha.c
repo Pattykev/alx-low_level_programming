e11b49f5a72bca701bdb4b21f057877b36a8ed79
Pattykev <trishasoftwareengineer@gmail.com>
2023-03-21 03:03:34 -0700

    style: add comments

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,21 @@
 #include"main.h"
 
 /**
- * main - Entry point
- *
- * Description: print if the number is a lower character
+ * _isalpha: print if the number is a character
  *           
+ * @c parameter
  *
  * Return: Always 0 (Success)
 */
 
 int _isalpha(int c)
 {
-      if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-      {
-            return (1);
-      }
-      else
-      {
-            return (0);
-      }  
+	 if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+	       	return (1);
+	}
+	 else
+	 {
+		 return (0)
+	 } 
 }
