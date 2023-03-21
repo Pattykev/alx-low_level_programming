903883b5f613e184df658941095a9a26a3a0ec8e
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-21 08:41:17 +0100

    feat: print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n=97;
+
+	while(n <= 122)
+  {
+		putchar(n);
+    n++;
+  }
+
+	return (0);
+}
