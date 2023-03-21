936a54f5668fe638fd50776462c7f9852151656e
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-21 09:38:51 +0100

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * print_last_digit - print last digit of a number.
+ *
+ * @n: takes number input
+ *
+ * Return: lastDigit
+*/
+
+int print_last_digit(int c)
+{
+int lastDigit;
+if(n < 0)
+{
+lastDigit = (-1 * n) % 10;  
+}
+lastDigit = (-1 * n) % 10; 
+return (lastDigit);
+}  
