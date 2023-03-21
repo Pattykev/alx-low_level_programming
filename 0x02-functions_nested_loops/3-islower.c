6600a5e84e0e8213e7f2a8ca3a8b63c635028bf8
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-21 09:07:09 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: print if the number is a lower character
+ *           
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _islower(int c)
+{
+      if(c >= 97 && c <= 122)
+       {
+        return (1);
+  }
+  else
+  {
+    return (0);
+  }  
+}
