920eda4aaca2d3efc7ff576a4bb58ac15c2b74cd
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-22 20:42:16 +0100

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description - print a fibonacci numbers
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	unsigned long cal = 2, i, a, num = 1;
+
+	printf("%lu, %lu, ", num, cal);
+	for (i = 1; i <= 48; i++)
+	{
+		a = num;
+		num = cal;
+		cal += a;
+		if (i < 48)
+			printf("%lu, ", cal);
+		else
+			printf("%lu", cal);
+	}
+	printf("\n");
+	return (0);
+}
