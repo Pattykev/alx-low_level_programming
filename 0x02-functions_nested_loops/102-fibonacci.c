1749b2c50d08ec5733e2cbfdaa43aa654da444d1
Pattykev <trishasoftwareengineer@gmail.com>
2023-03-22 12:20:55 -0700

    fibonacci suite

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description - print a fibonacci numbers
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int cal = 2, i, num = 1;
+
+	printf("%d, %d, ", num, cal);
+	for (i = 1; i <= 47; i++)
+	{
+		num = cal;
+		cal += num;
+	}
+	printf("\n");
+	return (0);
+}
