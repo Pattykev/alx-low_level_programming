8acff0e3531955ec25e61a09a937c3a73af3e548
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-22 11:30:11 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop, the j is not incrementing
+* main - causes an infinite loop
 * Return: 0
 */
 
@@ -13,11 +13,11 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)
-        {
+      /**  while (i < 10)
+       * {
                 putchar(i);
-        }
-
+      *  }
+      */
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
