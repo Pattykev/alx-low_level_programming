085566c4100e4db4cd51b6ac8c550c18f0fe0b78
Pattykev <trishasoftwareengineer@gmail.com>
2023-03-22 02:19:50 -0700

    feat Style: times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include"main.h"
+
+/**
+ * print_times_table -  print an n times table
+ *
+ * @n: is the int parameter
+ *
+ */
+
+void print_times_table(int n)
+{
+	int mult, i, j;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+		_putchar(48);
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				mult = i * j;
+
+				if (mult <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (mult >= 10 && mult <= 99)
+					_putchar((mult / 10) + 48);
+				else
+				{
+					_putchar((mult / 100) + 48);
+					_putchar(((mult /10) % 10) + 48);
+				}
+
+				_putchar((mult % 10) + 48);		
+			}
+		_putchar('\n');	
+		}
+	}
+}	
