6ae5d15c189fbbe1c117b23e022b2499221b24d0
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-22 20:53:08 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,9 +11,8 @@
 
 int main(void)
 {
-	unsigned long cal = 2, i, a, num = 1;
+	unsigned long cal = 2, i, a, num = 1, sum = 2;
 
-	printf("%lu, %lu, ", num, cal);
 	for (i = 1; i <= 48; i++)
 	{
 		a = num;
