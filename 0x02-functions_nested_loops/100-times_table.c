d291fb97eb68ee81dc91504a8248e99111a67e7c
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-22 10:35:11 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,8 +14,7 @@ void print_times_table(int n)
 	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
-		{
-		        _putchar(48);
+		{       _putchar(48);
 			for (j = 1; j <= n; j++)
 			{
 				_putchar(',');
@@ -34,8 +33,7 @@ void print_times_table(int n)
 				{
 					_putchar((mult / 100) + 48);
 					_putchar(((mult / 10) % 10) + 48);
-				}
-				_putchar((mult % 10) + 48);		
+				} _putchar((mult % 10) + 48);		
 			} _putchar('\n');	
 		}
 	}
