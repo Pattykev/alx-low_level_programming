ca85926364fb71578be56de5e61635b094d757db
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-23 12:51:33 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - multiply two number
+ *
+ * @a: integer integer
+ * @b: another integer
+ *
+ * Return: the product of two integers
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
