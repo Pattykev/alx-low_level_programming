89868f40f63a6b0a027813487d165dd37ba3563d
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-23 12:47:18 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,12 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void prints_numbers(void);
+void print_more_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
 
 #endif
