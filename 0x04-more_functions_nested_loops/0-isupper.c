5699aeff53506325a5955c7b4c390e48069c54d9
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-23 12:25:39 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,6 +3,8 @@
 /**
  * _isupper - if a number is a upper or lower
  *
+ * @c: parameter
+ *
  * Return: 1 if upper and 0 if lower
  *
  */
@@ -11,6 +13,6 @@ int _isupper(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (0);
-  if (c >= 65 && c <= 90)
-    return (1);
+	else
+		return (1);
 }
