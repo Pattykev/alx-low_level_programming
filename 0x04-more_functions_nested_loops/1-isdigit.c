0c344684247c5e000bc263973a5aa7ca502113d4
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-23 12:40:16 +0100

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+ * _isdigit - check if c is a digit
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if it's digit, 0 if not
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
