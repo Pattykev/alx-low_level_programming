3a8e4fbd5af6637a59ce79b0ac00c3ff3effdf87
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-23 13:09:12 +0100

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_numbers - print 0 - 9 except 2 and 4
+ *             only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+    if (num == 2 || num == 4)
+      continue;
+    else
+		  _putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
