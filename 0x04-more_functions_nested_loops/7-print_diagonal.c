23aea354fd5b1061a457914375b921800d5e1a44
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-24 08:54:58 +0100

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include ""main.h"
+
+/**
+ * print_diagonal - enter point of the functions
+ *
+ * @n: integer parameter
+ *
+ * Return: nothing
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+    _putchar('\');
+	}
+	_putchar('\n');
+}
