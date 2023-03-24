933f421672bdb4faeff5d019e21e91645ea90ce1
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-24 09:31:29 +0100

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 - 100 for 3 multiple print
+ *                 'Fizz', 5 multiple print 'Buzz' and 3 and 5 multiple 'FizzBuzz'
+ *                 use printf()
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+    if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz ");
+    else if (num % 5 == 0)
+      printf("Buzz ");
+    else if (num % 3 == 0)
+      printf("Fizz ");
+    else
+      printf("%d ", num);
+	}
+	return (0);
+}
