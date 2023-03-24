0fb54a28a20b83f51731db21353b6029ec00e97b
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-24 09:53:39 +0100

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * print_triangle - print a triangle
+ *
+ * @size: is the number of lines
+ *     should be printed
+*/
+
+void print_triangle(int size)
+{
+	int postn, space, diez;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= size; postn++)
+		{
+			for (space = size; space > size-postn; space--)
+				_putchar(' ');
+      for (diez = 0; diez < postn; diez++)
+				_putchar(35)
+			_putchar('\n');
+		}
+	}
+}
