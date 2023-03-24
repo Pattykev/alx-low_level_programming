b872b33cf783fe3275b17a69d32dca2194be358b
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-24 09:02:28 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,21 +5,25 @@
  *
  * @n: integer parameter
  *
- * Return: nothing
- *
  */
 
 void print_diagonal(int n)
 {
 	int i, j;
-
-	for (i = 0; i < n; i++)
+        if (n == 0)
 	{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(' ');
-		}
-   		 _putchar(92);
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+   		 	_putchar(92);
+		}
+		_putchar('\n');
+	}
 }
