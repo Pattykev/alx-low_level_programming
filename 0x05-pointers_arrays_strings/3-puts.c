aad5674ea2f9f0f869f917dac9c063e30e0c8e4d
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-28 13:19:40 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,10 +10,11 @@
 
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+	while (*(str + i) != '\0')
 	{
-		_putchar(*str + 0);
-		++str;
+		_putchar(*(str + i) + 0);
+		++i;
 	}
 	_putchar('\n');
 }
