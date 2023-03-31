c13d603c8025d5154a653a41773b3421e793754d
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-31 14:08:32 +0100

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+/**
+ * cap_string - a function that changes all
+ *             lowercase letters of a string
+ *             to uppercase.
+ *
+ * @s : pointer to input char
+ *
+ * Return: @s
+*/
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '\"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}') && s[i++] != ' ')
+			s[i++] = s[i++] - 32;
+    if (s[i] == ' ')
+      s[i++] = s[i++] - 32;
+		i++;
+	}
+	return (s);
+}
