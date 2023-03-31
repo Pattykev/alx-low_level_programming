4ce3d12119853399cd3a5d629172f4a9ac1a7597
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-31 13:11:51 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,12 @@ void reverse_array(int *a, int n)
 {
 	int i, temp, b;
 	
-	b=n;
+	b=n-1;
 	for (i =0; i < n; i++)
   	{
-		b--;
 		temp = a[b];
     		a[b] = a[i];
     		a[i] = temp;
+		b--;
 	}
 }
