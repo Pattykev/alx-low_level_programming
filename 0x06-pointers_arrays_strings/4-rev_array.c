9bfb1146f3190d3fcbabd5408717c51cd36fe3e3
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-03-31 13:13:11 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,11 @@
 /**
- * reverse_array - a function to reverse an array
+ * reverse_array - a function that reverses the
+ *                content of an array of integers
  *
- * @a: array to reverse
- * @n: the number of elements of the array 
+ * @a: pointer to int array
+ * @n: is the number of elements to swap
  *
+ * Return: nothing
 */
 
 void reverse_array(int *a, int n)
