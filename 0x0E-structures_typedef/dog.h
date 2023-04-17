413fd57b620aef380022b505a2bdc9a6d014491a
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-04-17 16:36:08 +0100

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - a structure which describe a dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: a dog must may have a name, an age and an owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
