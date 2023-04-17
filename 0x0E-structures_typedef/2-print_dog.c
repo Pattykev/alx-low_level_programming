caeff4c857f5b638b822beed7c160840d1397c1f
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-04-17 17:18:25 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,20 +8,16 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-    if (d.name == NULL)
-      printf("Name: (nil)");
-    else
-      printf("Name: %s \n", d.name);
+   		 if (d.name == NULL)
+      			printf("Name: (nil)");
+    		 else
+   			printf("Name: %s \n", d.name);
+      		 printf("Age: %s \n", d.age);
     
-    if (d.age == NULL)
-      printf("Age: (nil)");
-    else
-      printf("Age: %s \n", d.age);
-    
-    if (d.owner == NULL)
-      printf("Owner: (nil)");
-    else
-      printf("Owner: %s \n", d.owner);
+    		 if (d.owner == NULL)
+      			printf("Owner: (nil)");
+    		 else
+      			printf("Owner: %s \n", d.owner);
 		
 	}
 }
