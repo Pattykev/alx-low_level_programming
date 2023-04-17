7f58c8e2baca67fb9baf554b10f58b55330ba922
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-04-17 17:05:37 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "dog.h"
-
 /**
 *init_dog - initiate a variable of type struct dog
 *@d: the variable
@@ -8,7 +7,6 @@
 *@age: the age of the dog
 *@owner: the owner of the dog
 */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
   if (d != NULL)
