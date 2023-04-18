4a97e74972a0b9a325d0b1b71eb197df6f11c1bb
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-04-18 22:22:12 +0100

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+*main - return the name of the compile function
+*@argc: the number of parameters past to the program
+*@argv: an array of parameter
+*
+*/
+
+int main(int argc __attribute__((unsed)), char *argv[])
+{
+  printf("%s\n",argv[0]);
+  return (0); 
+}
