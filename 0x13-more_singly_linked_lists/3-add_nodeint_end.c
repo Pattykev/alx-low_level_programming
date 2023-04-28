76907eace0eb73faf52faef38d2d955a65596b32
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-04-28 22:59:36 +0100

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,17 +9,17 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-  listint_t *end;
-  listint_t *tmp;
+        listint_t *end;
+        listint_t *tmp;
   
-  end =  malloc(sizeof(listint_t));
-  if (end == NULL)
-    return NULL;
-  end->n = n;
-  tmp = *header;
-  while (tmp->next != NULL )
-    tmp = tmp->next;
-  tmp->next = end;
+        end =  malloc(sizeof(listint_t));
+        if (end == NULL)
+                return NULL;
+        end->n = n;
+        tmp = *header;
+        while (tmp->next != NULL )
+                tmp = tmp->next;
+        tmp->next = end;
   
-  return (end);
+        return (end);
 }
