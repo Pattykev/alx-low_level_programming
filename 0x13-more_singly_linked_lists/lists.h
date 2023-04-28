c472b9364e1a5f6b0e335c70d8812da1e426e1dc
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-04-28 22:08:20 +0100

    Create lists.h

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,9 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+
+#endif
