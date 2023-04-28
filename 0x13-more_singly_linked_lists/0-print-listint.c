fc95a29d5d6b1fd18cedcada10c3fbaa1ee951ad
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-04-28 22:22:30 +0100

    Create 0-print-listint.c

---
+++ 0x13-more_singly_linked_lists/0-print-listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+*print_listint - prints data form a linked list
+*@h: the struct linked list
+*Return: size_t
+*/
+
+size_t print_listint(const listint_t *h)
+{
+  size_t num = 0;
+  
+  while (h != null)
+  {
+    num++;
+    printf("%d", h->n);
+    h = h->next;
+  }
+  
+  return (num);
+}
