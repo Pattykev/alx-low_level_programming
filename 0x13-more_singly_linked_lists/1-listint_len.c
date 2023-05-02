d509e811bb424431c6f0fb24fefb91b7c8d0bc0d
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-05-02 23:40:59 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,18 @@
 
 size_t listint_len(const listint_t *h)
 {
-  size_t num = 0;
-  
-  while (h-> != NULL)
-  {
-    num++;
-    h = h->next;
-  }
-  
-  return (num);
+	size_t elements = 1;
+
+	/*return 0 as no of elements when h is NULL*/
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		if (h->n != '\0')
+			elements++;
+		h = h->next;
+	}
+
+	return (elements);
 }
