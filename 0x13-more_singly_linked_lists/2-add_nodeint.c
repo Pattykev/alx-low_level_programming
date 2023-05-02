eeaa34826fb433c5dce6c8387a27857f4ce1de96
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-05-02 23:44:35 +0100

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,17 +7,25 @@
 *Return: listint_t
 */
 
+
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-  listint_t *begin;
-  
-  begin = malloc(sizeof(listint_t));
-  
-  if (begin == NULL)
-    return NULL;
-  begin->n = n;
-  begin->next = *head;
-  *head = begin;
-  
-  return begin;
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/*link first node in head with the new_node*/
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	new_node->n = n;
+	/*add new node at the beginning of the list*/
+	*head = new_node;
+
+	return (*head);
 }
